f8305ee57fc35b53abef783f30a1f7058307b4d1
Klis18 <klisnp18@gmail.com>
2025-02-25 12:45:55 -0500

    carga masiva de datos de asegurados en excel terminado

---
+++ src/app/insured/components/mass-loading-insured/mass-loading-insured.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import * as XLSX from 'xlsx';
+import { InsuredService } from '../../services/insured.service';
+import { Insured } from '../../interfaces/insured.interface';
+
+@Component({
+  selector: 'app-mass-loading-insured',
+  standalone: true,
+  imports: [],
+  templateUrl: './mass-loading-insured.component.html',
+  styles: ``
+})
+export class MassLoadingInsuredComponent {
+
+  constructor(private insuredService: InsuredService){}
+
+  addMasiveInsured(insured:Insured){
+    console.log('Insured agregado por carga masiva',insured);
+      this.insuredService.addInsured(insured);
+  }
+  
+  readExcel(event:any):void{
+    const archivo = event.target.files[0];
+
+    if(archivo){
+      const lector = new FileReader();
+
+      lector.onload = (e : any) => {
+        const datos = e.target.result;
+        const libro = XLSX.read(datos, {type: 'array'});
+
+        const hoja = libro.Sheets[libro.SheetNames[0]];
+        const datosExcel = XLSX.utils.sheet_to_json(hoja, {header:1});
+
+        datosExcel.forEach((fila:any, index:number) => {
+          if(index>0 && fila.length === 4){
+            const id = fila[0];
+            const name = fila[1];
+            const phone = fila[2];
+            const age = fila[3];
+            const insuredData = {id,name,phone,age};
+            this.addMasiveInsured(insuredData);
+          }else {
+            console.log(`Fila ${index + 1} no tiene 4 columnas.`);
+          }
+        });
+      };
+      lector.readAsArrayBuffer(archivo);
+    }
+  }
+
+}
--- src/app/insured/pages/insured/insured.component.ts
+++ src/app/insured/pages/insured/insured.component.ts
@@ -5,19 +5,25 @@ import { InsuredTableComponent } from '../../components/insured-table/insured-ta
 import { InsuredModalComponent } from '../../components/insured-modal/insured-modal.component';
 import { MatDialog } from '@angular/material/dialog';
 
+import * as XLSX from 'xlsx';
+import { InsuredService } from '../../services/insured.service';
+import { Insured } from '../../interfaces/insured.interface';
+import { MassLoadingInsuredComponent } from '../../components/mass-loading-insured/mass-loading-insured.component';
+
 @Component({
   selector: 'app-insured',
   standalone: true,
   imports: [
     MatIconModule,
-    InsuredTableComponent
+    InsuredTableComponent,
+    MassLoadingInsuredComponent
   ],
   templateUrl: './insured.component.html',
   styleUrl: './insured.component.css'
 })
 export class InsuredComponent {
 
-  constructor(private dialog: MatDialog){}
+  constructor(private dialog: MatDialog, private insuredService: InsuredService){}
 
   addInsured(){
     const dialogRef = this.dialog.open(InsuredModalComponent,{
@@ -32,4 +38,5 @@ export class InsuredComponent {
           console.log('Modal agregar, cerrado')
         })
   }
+
 }
