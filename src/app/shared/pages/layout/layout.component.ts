60457d768b9650136f635e5a810ab57e1260b6e5
Klis18 <klisnp18@gmail.com>
2025-02-27 18:35:47 -0500

    modificaciones en menú

--- src/app/shared/pages/layout/layout.component.ts
+++ src/app/shared/pages/layout/layout.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../../components/menu/menu.component';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [
     MenuComponent,
-    RouterModule
+    RouterModule,
+    MatIconModule
   ],
   templateUrl: './layout.component.html',
   styles: ``
 })
 export class LayoutComponent {
 
+  public menuVisible : boolean = false;
+
+
+  showMenu(){
+    this.menuVisible = !this.menuVisible;
+  }
 }
