9e4f9106e200780a2d330aa8b669430301e0ea52
Klis18 <klisnp18@gmail.com>
2025-02-28 16:38:55 -0500

    validaciones agregadas en formulario de asegurados

--- src/app/insured/components/insured-modal/insured-modal.component.ts
+++ src/app/insured/components/insured-modal/insured-modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { InsuredService } from '../../services/insured.service';
@@ -26,10 +26,10 @@ export class InsuredModalComponent implements OnInit{
 
   ngOnInit(): void {
     this.insuredForm = this.fb.group({
-      insuredId : ['', [Validators.required, Validators.minLength(10)]],
+      insuredId : ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
       insuredName:['', [Validators.required]],
-      phone:['', [Validators.required]],
-      age:['', [Validators.required]]
+      phone:['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+      age:['', [Validators.required, Validators.min(18)]]
     });
 
     this.data.type == 'edit' ? this.typeForm = 'Editar' : this.typeForm = 'Agregar';
@@ -41,7 +41,9 @@ export class InsuredModalComponent implements OnInit{
   }
 
   onSubmit(){
-    this.data.type == 'edit' ? this.editInsured() : this.addInsured();
+    if(this.insuredForm.valid){
+      this.data.type == 'edit' ? this.editInsured() : this.addInsured();
+    }
   }
 
   addInsured(){
@@ -60,5 +62,38 @@ export class InsuredModalComponent implements OnInit{
 
   closeForm(){
     this.dialogRef.close()
+  }
+
+  verifyError(campoName: string):boolean{
+    const controlForm = this.insuredForm.get(campoName);
+    if(controlForm?.invalid && controlForm.touched){
+      return true;
+    }
+    return false;
+  }
+
+
+  showFormsErrorMessages(campoName: string){
+    const controlForm = this.insuredForm.get(campoName);
+
+    if(!controlForm) return '';
+
+    const errors = controlForm.errors;
+
+    const errorMessages:any = {
+      required:'Este campo es obligatorio',
+      minlength: `El valor mínimo de caracteres es ${errors?.['minlength']?.requiredLength}`,
+      maxlength: `El valor máximo de caracteres es ${errors?.['maxlength']?.requiredLength}`,
+      min: `La edad mínima permitida es ${errors?.['min']?.min}`
+    }
+
+    for(const error in controlForm.errors){
+      if(controlForm.errors.hasOwnProperty(error)){
+
+        return errorMessages[error] || 'Error desconocido';
+      }
+
+      return '';
+    }
   }
 }
